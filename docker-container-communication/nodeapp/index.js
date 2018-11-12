2c53245fc598f1bd4714e4cd0e599c66875b5b83
Rumen Neshev <rstar2@abv.bg>
2018-11-12 23:12:04 +0200

    asd

--- docker-container-communication/nodeapp/index.js
+++ docker-container-communication/nodeapp/index.js
@@ -6,24 +6,25 @@ const db = require('./db');
 
 const parseQueryMiddleware = (req, res, next) => {
     req.query = {};
+    req.query.uid = '5be9e9559efede4a396a1796';
     next();
 };
 
 const dbMiddleware = (req, res, next) => {
-    if (req.query.uid) {
-        db.getUser(req.query.uid)
-            .then(user => {
-                req.query.name = user.name;
+    const uid = req.query.uid;
+    if (uid) {
+        console.log(`Getting user for ${uid}`);
+        db.getUser(uid)
+            .then(({ name }) => {
+                console.log(`User name for ${uid} is ${name}`);
+                req.query.name = name;
                 next();
             })
             .catch(() => next());
     } else {
         next();
     }
-
-
 };
-
 
 const handleMiddleware = (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
