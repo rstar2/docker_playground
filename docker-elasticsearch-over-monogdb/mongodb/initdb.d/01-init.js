cd7db85de496417f282c1323147b894a3b2b4d53
Rumen Neshev <rstar2@abv.bg>
2019-01-14 00:22:18 +0200

    asd

--- docker-elasticsearch-over-monogdb/mongodb/initdb.d/01-init.js
+++ docker-elasticsearch-over-monogdb/mongodb/initdb.d/01-init.js
@@ -1,11 +1,20 @@
 /* global db*/
 
-// Insert 2 init users
+// Insert a demo data - 2 articles
+db.createCollection('articles');
 
-db.users.insert({
-    name: 'Rumen'
+// Insert 2 init users
+db.articles.insert({
+    title: 'Yahoo sale to Verizon',
+    content: 'The sale is being done in two steps. The first step will be the transfer of any assets related to Yahoo business to a singular subsidiary. This includes the stock in the business subsidiaries that make up Yahoo that are not already in the single subsidiary, as well as the odd assets like benefit plan rights. This is what is being sold to Verizon. A license of Yahoo’s oldest patents is being held back in the so-called Excalibur portfolio. This will stay with Yahoo, as will Yahoo’s stakes in Alibaba Group and Yahoo Japan.'
 });
 
-db.users.insert({
-    name: 'Kasia'
+db.articles.insert({
+    title: 'Chinese Group to Pay $4.4 Billion for Caesars Mobile Games',
+    content: 'In the most recent example in a growing trend of big deals for smartphone-based games, a consortium of Chinese investors led by the game company Shanghai Giant Network Technology said in a statement on Saturday that it would pay $4.4 billion to Caesars Interactive Entertainment for Playtika, its social and mobile games unit. Caesars Interactive is controlled by the owners of Caesars Palace and other casinos in Las Vegas and elsewhere.'
+});
+
+db.articles.createIndex({
+    title: 'text',
+    content: 'text'
 });
