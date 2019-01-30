7d834a86436ac5482772ef3361fd44f3a8ef718e
Rumen Neshev <rstar2@abv.bg>
2019-01-30 23:46:28 +0200

    correct file modes

--- docker-elasticsearch-over-monogdb/elasticsearch/config/setup.sh
+++ docker-elasticsearch-over-monogdb/elasticsearch/config/setup.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-echo Initiating Elasticsearch Custom Index
+echo Initiating Elasticsearch
 # move to the directory of this setup script
-cd "$(dirname "$0")"
+echo $(dirname "$0")
 
-# for some reason even when port 9200 is open Elasticsearch is unable to be accessed as authentication fails
-# a few seconds later it works
-until $(curl -sSf -XGET --insecure --user elastic:changeme 'http://localhost:9200/_cluster/health?wait_for_status=yellow' > /dev/null); do
-    printf 'AUTHENTICATION ERROR DUE TO X-PACK, trying again in 5 seconds \n'
-    sleep 5
-done
-
-# create a new index with the settings in es_index_config.json
-curl -v --insecure --user elastic:changeme -XPUT '0.0.0.0:9200/test?pretty' \
+# create a new index with the settings in index_articles.json
+curl -X PUT http://localhost:9200/articles \
      -H 'Content-Type: application/json' \
-     -d @es_index_config.json
+     -d @index_articles.json
