e7136be6e73537d2269b21b78104b0b19042ee63
rstar2 <rstar2@abv.bg>
2019-01-31 11:23:07 +0200

    asd

--- docker-elasticsearch-over-monogdb/README.md
+++ docker-elasticsearch-over-monogdb/README.md
@@ -5,5 +5,13 @@
 
 Note : for Windows in order PWD to work it must be set like this: ``` $ set PWD=%cd% ```
 
-##
-TODO: https://medium.com/@xoor/indexing-mongodb-with-elasticsearch-2c428b676343
+## Configure a ElasticSearch index
+
+1. Will use the name 'website_opt' (optimized) as later the MongoDB collection 'website' will be imported in ElasticSearch into the index also called 'website'
+
+2. There's a startup configuration script in elasticsearch/config/setup.sh that will create this index automatically
+
+## Importing from MongoDB into ElasticSearch
+
+> Inspired/Learned by : https://medium.com/@xoor/indexing-mongodb-with-elasticsearch-2c428b676343
+
--- docker-elasticsearch-over-monogdb/elasticsearch/config/setup.sh
+++ docker-elasticsearch-over-monogdb/elasticsearch/config/setup.sh
@@ -2,9 +2,35 @@
 
 echo Initiating Elasticsearch
 # move to the directory of this setup script
-echo $(dirname "$0")
+cd $(dirname "$0")
 
-# create a new index with the settings in index_articles.json
-curl -X PUT http://localhost:9200/articles \
-     -H 'Content-Type: application/json' \
-     -d @index_articles.json
+# Normally when the script is executed by the ElasticSearch container itself this will be 'localhost'
+# as it connects to itself.
+# But with the adding of the ELASTICSEARCH_IP/ELASTICSEARCH_PORT variables they could be passed as environment vars
+# like when using from Windows docker machine (set ELASTICSEARCH_IP=%docker-machine ip%)
+# or when binding the container's port to the host port not on 9200
+elasticsearh_ip=${ELASTICSEARCH_IP:-"localhost"}
+elasticsearh_port=${ELASTICSEARCH_PORT:-"9200"}
+elasticsearch_url=http://$elasticsearh_ip:$elasticsearh_port
+echo Elasticsearch is accessible on: ${elasticsearch_url}
+
+# check if required index is already existing and if not the create it
+# -s --> silien, no progress
+# -o --> write to file instad of stdout (in this case ignore with /dev/null)
+# -w --> display information on stdout after a completed transfer (there are variables available like http_code)
+exists=$(curl --head -s -o /dev/null -w %{http_code} ${elasticsearch_url}/website_opt)
+# This should return just the status code - 200 means (index exists)
+
+if (($exists != 200)); then
+    echo Creating and configuring the 'website_opt' index
+
+    # create a new index with the settings in index_website_opt.json
+    curl -X PUT ${elasticsearch_url}/website_opt \
+        -H 'Content-Type: application/json' \
+        -d @index_website_opt.json
+
+    echo Created and configured the 'website_opt' index
+else
+    echo Already created 'website_opt' index
+fi
+
