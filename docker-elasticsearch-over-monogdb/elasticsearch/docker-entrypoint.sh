21eedacdb2565327b41ca1d75142d7be7ee81a86
Rumen Neshev <rstar2@abv.bg>
2019-02-01 00:35:07 +0200

    asd

--- docker-elasticsearch-over-monogdb/elasticsearch/docker-entrypoint.sh
+++ docker-elasticsearch-over-monogdb/elasticsearch/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# wait for Elasticsearch to start, then run the setup script to
-# create and configure the index.
-exec /usr/share/elasticsearch/utils/wait-for-it.sh localhost:9200 -- /usr/share/elasticsearch/config/setup.sh &
+# execute as background task that waits for Elasticsearch to start,
+# and then runs the setup script to create and configure the index.
+exec /usr/share/elasticsearch/utils/wait-for-it.sh localhost:9200 -- /usr/share/elasticsearch/setup/setup.sh &
+
+# this will start the CMD which is by default 'elasticsearch'
 exec $@
