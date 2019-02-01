2a2349f283425131268f7bc8d56edc493c51862f
rstar2 <rstar2@abv.bg>
2019-02-01 17:18:51 +0200

    asd

---
+++ docker-elasticsearch-over-monogdb/mongodb-connector/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mongo="${MONGO:-mongo:27017}"
+elasticsearch="${ELASTICSEARCH:-elasticsearch:9200}"
+
+
+
+mongo-connector --auto-commit-interval=0 -m ${mongo} -t ${elasticsearch} -d elastic2_doc_manager
