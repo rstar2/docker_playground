44ae54acaeade224e56dc83b8233725e15c5b9ae
rstar2 <rstar2@abv.bg>
2019-02-04 14:51:40 +0200

    asd

--- docker-elasticsearch-over-monogdb/mongodb-connector/docker-entrypoint.sh
+++ docker-elasticsearch-over-monogdb/mongodb-connector/docker-entrypoint.sh
@@ -3,4 +3,5 @@
 mongo="${MONGO:-mongo:27017}"
 elasticsearch="${ELASTICSEARCH:-elasticsearch:9200}"
 
-mongo-connector --auto-commit-interval=0 -m ${mongo} -t ${elasticsearch} -d elastic2_doc_manager
+mongo-connector --auto-commit-interval=0 -m ${mongo} -t ${elasticsearch} -d \
+                elastic2_doc_manager -n website.articles -g website_opt.articles
--- docker-elasticsearch-over-monogdb/mongodb/initdb.d/01-replica.sh
+++ docker-elasticsearch-over-monogdb/mongodb/initdb.d/01-replica.sh
@@ -2,5 +2,21 @@
 # Note ".sh" files are just excecuted by the mongo conatiner on startup,
 # while ".js" files are opassed to the mongo shell (e.g. mongo ... $MONGO_INITDB_DATABASE file.js)
 
+hostname=${MONGO_RS_HOST:-$(hostname)}
+
 # Call the real JS file but it should NOT be with ".js" extension otherwise it will be executed also
-mongo --eval "var MONGO_RS_HOST = '$MONGO_RS_HOST'" 01-replica-js
+mongo 127.0.0.1:27017 --eval "var MONGO_RS_HOST = '$hostname'" /docker-entrypoint-initdb.d/01-replica-js
+
+
+# Onother option for using a shell script that calls the mongo shell:
+# mongo <<EOF
+# use admin
+# db.createUser({
+#   user:  '$ENV_APP_SERVER_USERNAME',
+#   pwd: '$ENV_APP_SERVER_PW',
+#   roles: [{
+#     role: 'readWrite',
+#     db: 'dummydb'
+#   }]
+# })
+# EOF
